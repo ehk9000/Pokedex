784ec23aba099b1262aaf93a29fc360cb97fd34b
Erik Konner <erikkonner@gmail.com>
2019-05-30 12:19:20 -0600

    Imported provider, broswer router in index.js and updated favicon

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './styles/index.scss';
+import App from './App/App';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { createStore, applyMiddleWare } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// const store = createStore(rootReducer, composeWithDevTools(applyMiddleWare(thunk)))
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const router = (
+  // <Provider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  // </Provider>
+);
+
+ReactDOM.render(router, document.getElementById('root'));
+
