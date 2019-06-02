3be77eb5a5c4e633118567f80b64074bee960800
Erik Konner <erikkonner@gmail.com>
2019-06-02 14:16:48 -0600

    Snapshot tests for App and Controls

--- src/components/Controls/Controls.js
+++ src/components/Controls/Controls.js
@@ -4,7 +4,7 @@ import { Route } from 'react-router-dom';
 export const Controls = () => {
   return (
     <section>
-      <Route exact path='/' component={PokemonContainer} />
+      {/* <Route exact path='/' component={PokemonContainer} /> */}
     </section>
   )
 }
