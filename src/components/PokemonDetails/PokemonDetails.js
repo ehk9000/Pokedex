bd3d4714fcefacb3054436b9fcaaa3214c0df8ab
Erik Konner <erikkonner@gmail.com>
2019-06-02 17:26:25 -0600

    pokemon displaying on dom

--- src/components/PokemonDetails/PokemonDetails.js
+++ src/components/PokemonDetails/PokemonDetails.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { Link }  from 'react-router-dom';
 
-export const PokemonDetails = () => {
+export const PokemonDetails = (props) => {
+  let { name, sprites, types} = props;
   return (
     <article>
+      <h3>{name}</h3>
+      <img src={sprites.front_default} alt="pokemon"/>
+      <p>{types[0].type.name}</p>
 
     </article>
   )
