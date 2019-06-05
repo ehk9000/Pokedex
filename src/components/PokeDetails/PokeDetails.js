c8e691bba99eba6c25650fd1498803218a43621b
Erik Konner <erikkonner@gmail.com>
2019-06-04 19:12:14 -0600

    Updated Font. pokedetails now flexed and displaying correctly

--- src/components/PokeDetails/PokeDetails.js
+++ src/components/PokeDetails/PokeDetails.js
@@ -3,42 +3,43 @@ import './PokeDetails.scss';
 
 const PokeDetails = ({ name, images, abilities, stats, types, weight, height}) => {
   let displayTypes = types.map(type => {
-    return <li>{type}</li>
+    return <li className="type-style">{type}</li>
   });
   let displayAbilities = abilities.map(ability => {
-    return <li>{ability}</li>
+    return <li className="type-style">{ability}</li>
       
   });
   let displayStats = stats.map(stat => {
     return <ul>
-            <li>{stat.name}</li>
-            <li>{stat.base}</li>
-
-            </ul>
+              <li>{stat.name}: </li>
+              <li>{stat.base}</li>
+          </ul>
   });
   
 
   return (
     <article className="poke-details-wrapper">
-      <h3>{name}</h3>
+      <h2>{name}</h2>
       <img src={images.back} alt={`${name} back view`} className="details-img"/>
       <img src={images.front} alt={`${name} front view`} className="details-img"/>
-      <section className="stats-wrapper">
-        <h3>Type:</h3>
-        <ul>
-          {displayTypes}  
+      <div className="details-flex">
+        <section className="stats-wrapper">
+          <h3 className="details-header">Type:</h3>
+          <ul>
+            {displayTypes}  
+          </ul>
+          <h3 className="details-header">Abilities:</h3>
+          <ul>
+            {displayAbilities}
+          </ul>
+          <ul>
+            <li>Weight: {weight}</li>
+            <li className="height-style">Height: {height}</li>
+          </ul>
+            {displayStats}
+        </section>
 
-        </ul>
-        <h3 className="abilities-title">Abilities:</h3>
-        <ul>
-          {displayAbilities}
-        </ul>
-        <ul>
-          <li>Weight: {weight}</li>
-          <li>Height: {height}</li>
-        </ul>
-          {displayStats}
-      </section>
+      </div>
     </article>
   )
 }
